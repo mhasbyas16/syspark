0e3c4dc21523eda2ee74eaae6f7b7e18e564b7f2
unknown <hasbyas16@gmail.com>
2019-06-10 15:23:39 +0700

    sessions

---
+++ storage/framework/views/d3fc2670dfe9cc6fe252c9591029fc05b45e215a.php
@@ -0,0 +1,132 @@
+<?php $__env->startSection('isi'); ?>
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Content Row -->
+          <div class="row">
+
+                <!-- Illustrations -->
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Welcome</h6>
+                </div>
+                <div class="card-body">
+                  <div class="text-center">
+                    <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="img/undraw_posting_photo.svg" alt="">
+                  </div>
+                  <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>
+                </div>
+              </div>
+          </div>
+
+          <!-- Content Row -->
+          <div class="row">
+            <div class="col-lg-6 mb-4">
+              <!-- Project Card Example -->
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Parking Location Status</h6>
+                </div>
+                <div class="card-body">
+                  <h4 class="small font-weight-bold">Basement <span class="float-right">Complete!</span></h4>
+                  <div class="progress mb-4">
+                    <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                  <h4 class="small font-weight-bold">Lantai 1 <span class="float-right">80%</span></h4>
+                  <div class="progress mb-4">
+                    <div class="progress-bar bg-warning" role="progressbar" style="width: 80%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                  <h4 class="small font-weight-bold">Lantai 2 <span class="float-right">40%</span></h4>
+                  <div class="progress mb-4">
+                    <div class="progress-bar bg-info" role="progressbar" style="width: 40%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                  <h4 class="small font-weight-bold">Lantai 3 <span class="float-right">20%</span></h4>
+                  <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 20%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-6 mb-4">
+
+              <!-- Color System -->
+              <div class="row">
+                <div class="col-lg-6 mb-4">
+                  <div class="card bg-primary text-white shadow">
+                    <div class="card-body" style="text-align: center;">
+                      Today's Parking Data
+                      <div class="text-white-50 small">...</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 mb-4">
+                  <div class="card bg-success text-white shadow">
+                    <div class="card-body form-inline" style="padding-right: 0px; margin-right: 0px;">
+                      <div class="col-md-6">
+                      100
+                      <div class="text-white-50 small">Motor</div>
+                      </div>
+                      <div class="col-md-4 text-right" style="float: right; padding-right: 0px;"><i class="fa fa-motorcycle fa-2x"></i></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 mb-4">
+                  <div class="card bg-info text-white shadow">
+                    <div class="card-body form-inline" style="padding-right: 0px; margin-right: 0px;">
+                      <div class="col-md-6">
+                      50
+                      <div class="text-white-50 small">Mobil</div>
+                      </div>
+                      <div class="col-md-4 text-right" style="float: right; padding-right: 0px;"><i class="fa fa-car fa-2x"></i></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 mb-4">
+                  <div class="card bg-warning text-white shadow">
+                    <div class="card-body form-inline" style="padding-right: 0px; margin-right: 0px;">
+                      <div class="col-md-6">
+                      10
+                      <div class="text-white-50 small">Truk</div>
+                      </div>
+                      <div class="col-md-4 text-right" style="float: right; padding-right: 0px;"><i class="fa fa-truck fa-2x"></i></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 mb-4">
+                  <div class="card bg-danger text-white shadow">
+                    <div class="card-body form-inline" style="padding-right: 0px; margin-right: 0px;">
+                      <div class="col-md-6">
+                      80
+                      <div class="text-white-50 small">Parkir Umum</div>
+                      </div>
+                      <div class="col-md-4 text-right" style="float: right; padding-right: 0px;"><i class="fab fa-buffer fa-2x"></i></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 mb-4">
+                  <div class="card bg-secondary text-white shadow">
+                    <div class="card-body form-inline" style="padding-right: 0px; margin-right: 0px;">
+                      <div class="col-md-6">
+                      80
+                      <div class="text-white-50 small">Parkir Member</div>
+                      </div>
+                      <div class="col-md-4 text-right" style="float: right; padding-right: 0px;"><i class="fa fa-book fa-2x"></i></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+
+  </div>
+  <!-- End of Main Content -->
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('temamenuheader', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\syspark\resources\views/dashboard.blade.php ENDPATH**/ ?>
