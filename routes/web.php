9b8f7b27a1129351e959c308ed337c557cffe87f
unknown <hasbyas16@gmail.com>
2019-06-10 12:30:48 +0700

    rubah web.php

--- routes/web.php
+++ routes/web.php
@@ -23,18 +23,35 @@ Route::get ('/signup','logincontroller@signup');
 //end
 
 //pegawai Controller
-Route::get('/petugas','pegawaicontroller@petugas');
+Route::get('/data_petugas','pegawaicontroller@petugas');
+Route::get('/detail_petugas/{id_petugas}','pegawaicontroller@detail_petugas');
+Route::post('/simpan_petugas','pegawaicontroller@simpan_petugas');
+Route::get('/delete_petugas/{id_petugas}','pegawaicontroller@delete_petugas');
+Route::get('/edit_petugas/{id_petugas}','pegawaicontroller@edit_petugas');
+Route::post('/simpan_edit_petugas','pegawaicontroller@simpan_edit_petugas');
+Route::get('/tambah_petugas','pegawaicontroller@tambah_petugas');
+//end
+
+//biaya Controller
+Route::get('/data_biaya','biayacontroller@biaya');
+Route::post('/simpan_biaya','biayacontroller@simpan_biaya');
+Route::get('/tambah_biaya','biayacontroller@tambah_biaya');
+Route::get('/delete_biaya/{id_biaya}','biayacontroller@delete_biaya');
+Route::get('/edit_biaya/{id_biaya}/{id_jenisk}','biayacontroller@edit_biaya');
+Route::post('/simpan_edit_biaya','biayacontroller@simpan_edit_biaya');
+//end
+
+//kendaraan Controller
+Route::get('/data_kendaraan','kendaraancontroller@data_kendaraan');
+Route::get('/tambah_kendaraan','kendaraancontroller@tambah_kendaraan');
+Route::get('/delete_kendaraan/{id_jenisk}','kendaraancontroller@delete_kendaraan');
+Route::get('/edit_kendaraan/{id_jenisk}','kendaraancontroller@edit_kendaraan');
+Route::post('/simpan_edit_kendaraan','kendaraancontroller@simpan_edit_kendaraan');
+Route::post('/simpan_kendaraan','kendaraancontroller@simpan_kendaraan');
 //end
 
 Route::get('/dash','Controller@Dashboard');//setelah login masuk dashboard
 Route::get('/main','Controller@Mainmenu');//akses main menu
-Route::post('/park','Controller@Park');//akses data profile
-
-
-Route::get('/profile/{a}','Controller@viewProfile');//akses profile menu
-Route::post('/saveProfile','Controller@saveProfile');//akses data profile
-
-
 Route::get('/gawai','Controller@Datapegawai');//akses data pegawai
 Route::get('/kendaraan','Controller@Datakendaraan');//akses data kendaraan
 Route::get('/parkir','Controller@Dataparkir');//akses data parkir
