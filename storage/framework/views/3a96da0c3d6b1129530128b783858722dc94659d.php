c6fe1d53e786c518003ee93f7c8325eb19b48992
unknown <hasbyas16@gmail.com>
2019-06-10 15:31:22 +0700

    session baru

--- storage/framework/views/3a96da0c3d6b1129530128b783858722dc94659d.php
+++ storage/framework/views/3a96da0c3d6b1129530128b783858722dc94659d.php
@@ -409,7 +409,7 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-<script src="<?php echo e(url('vendor/jquery/jquery.min.js')); ?>"></script>
+
   <script src="<?php echo e(url('vendor/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
 
   <!-- Core plugin JavaScript-->
