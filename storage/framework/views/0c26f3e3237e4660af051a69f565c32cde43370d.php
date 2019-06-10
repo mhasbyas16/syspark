18f0f41076f881dd42de4a9a1b3dd46d208d07e2
unknown <hasbyas16@gmail.com>
2019-06-10 15:29:00 +0700

    save baru lagi

--- storage/framework/views/0c26f3e3237e4660af051a69f565c32cde43370d.php
+++ storage/framework/views/0c26f3e3237e4660af051a69f565c32cde43370d.php
@@ -4,21 +4,29 @@
         <div class="container">
             <div class="row">
                 <!-- Page Heading -->
-                <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+                <h1 class="h3 mb-4 text-gray-800">Data Employee | Klik for Details</h1>
             </div>
             <div class="row justify-content-center">
+                <?php if(\Session::has('successalert')): ?>
+                    <div class="alert alert-success text-center">
+                        <strong> Success! </strong> <?php echo e(Session::get('successalert')); ?>
 
-          <table id="pegawai" class="display text-center" cellspacing="0" width="100%">
+                    </div>
+                    <?php elseif(\Session::has('alert')): ?>
+                        <div class="alert alert-danger text-center">
+                            <strong> Warning! </strong> <?php echo e(Session::get('alert')); ?>
+
+                        </div>
+                <?php endif; ?>
+
+          <table id="pegawai" class="display text-center nowrap" cellspacing="0" width="100%">
                   <thead>
                       <tr>
                           <th>ID Petugas</th>
                           <th>Nama</th>
-                          <th>NIK</th>
-                          <th>Gender</th>
-                          <th>Agama</th>
-                          <th>Alamat</th>
+                          <th>Email</th>
                           <th>No Telepon</th>
-                          <th>Foto</th>
+                          <th>Aksi</th>
                       </tr>
                   </thead>
 
@@ -26,12 +34,9 @@
                       <tr>
                           <th>ID Petugas</th>
                           <th>Nama</th>
-                          <th>NIK</th>
-                          <th>Gender</th>
-                          <th>Agama</th>
-                          <th>Alamat</th>
+                          <th>Email</th>
                           <th>No Telepon</th>
-                          <th>Foto</th>
+                          <th>Aksi</th>
                       </tr>
                   </tfoot>
 
@@ -39,19 +44,26 @@
                     <?php $__currentLoopData = $listP; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $listP): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                       <tr>
                           <td><?php echo e($listP->id_petugas); ?></td>
-                          <td><?php echo e($listP->nama); ?></td>
-                          <td><?php echo e($listP->nik); ?></td>
-                          <td><?php echo e($listP->jk); ?></td>
-                          <td><?php echo e($listP->agama); ?></td>
-                          <td><?php echo e($listP->alamat); ?></td>
+                          <td><a href="<?php echo e(url('/detail_petugas')); ?>/<?php echo e($listP->id_petugas); ?>"> <?php echo e($listP->nama); ?></a></td>
+                          <td><?php echo e($listP->email); ?></td>
                           <td><?php echo e($listP->no_tlfn); ?></td>
-                          <td><?php echo e($listP->pict); ?></td>
+                          <?php if($hakakses=='admin'): ?>
+                            <?php if($id_petugas==$listP->id_petugas): ?>
+                             <td></td>
+                            <?php else: ?>
+                            <td><a href="<?php echo e(url('/edit_petugas')); ?>/<?php echo e($listP->id_petugas); ?>"><i class="fas fa-user-edit text-red" style="color:green;font-size:20px;"></i></a> &nbsp;&nbsp;&nbsp;
+                                <a href="<?php echo e(url('/delete_petugas')); ?>/<?php echo e($listP->id_petugas); ?>" onclick="return confirm('Are You Sure Delete <?php echo e($listP->nama); ?> ?')"><i class="fas fa-trash-alt" style="color:red;font-size:20px;"></i></a>
+                            </td>
+                            <?php endif; ?>
+                          <?php else: ?>
+                          <td></td>
+                          <?php endif; ?>
                       </tr>
                      <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                   </tbody>
               </table>
 
-        </div>
+    </div>
         <!-- /.container-fluid -->
 
       </div>
